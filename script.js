2a385b0cace6a3d774d2c482e17da4501db05121
Adam <adamchrin8@gmail.com>
2025-04-18 19:17:46 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+})
